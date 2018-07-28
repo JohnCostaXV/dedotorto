7d7143d6430a29114f3c6f26cb17261aa05d9453
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-28 20:26:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ async def on_reaction_add(reaction, user):
      await client.add_roles(user, role)
      role1 = discord.utils.find(lambda r: r.name == "Sem registro", msg.server.roles)
      await client.remove_roles(user, role1)
-     await client.delete_message(msg)
 
      canal = client.get_channel('470361261930971148')
      embed = discord.Embed(
