821e6c4b74d4440c3d3e4b4e559848e4ecb2bc5b
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-07-28 23:14:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -426,8 +426,6 @@ async def on_reaction_add(reaction, user):
      embed.set_footer(text='Debuggers', icon_url='https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif')
      await client.send_message(canal, embed=embed)
 
-     time.sleep(10)
-     await client.delete_message(msg)
 
 
 
