fe8ea09b0312015a668c43a013961445f458046d
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-08-06 23:18:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -839,7 +839,7 @@ async def on_message(message):
         embed = discord.Embed(
             color=COR,
             description='**Informações**\n\n'
-                        'Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores chefiada por James Gosling,
+                        'Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores chefiada por James Gosling,'
                         'na empresa Sun Microsystems. Diferente das linguagens de programação convencionais, que são compiladas para código nativo, a linguagem Java é'
                         'compilada para um bytecode que é interpretado por uma máquina virtual (Java Virtual Machine, mais conhecida pela sua abreviação JVM). A linguagem'
                         'de programação Java é a linguagem convencional da Plataforma Java, mas não é a sua única linguagem.Java é uma linguagem de programação orientada'
